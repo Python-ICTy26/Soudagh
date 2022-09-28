dc06316fb8e20fe296ce9d6e4184c8daccb61d61
Soudagh <fr.kristina@inbox.ru>
2022-09-29 01:51:22 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,7 +15,21 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(plaintext)):
+        ch = ord(plaintext[i])
+        if 65 <= ch <= 90:
+            if ch + shift > 90:
+                ciphertext += chr(65 + (shift - (90 - ch)) - 1)
+            else:
+                ciphertext += chr(ch + shift)
+        elif 97 <= ch <= 122:
+            if ch + shift > 122:
+                ciphertext += chr(97 + (shift - (122 - ch)) - 1)
+            else:
+                ciphertext += chr(ch + shift)
+        else:
+            ciphertext += chr(ch)
+
     return ciphertext
 
 
@@ -34,6 +48,21 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+    for i in range(len(ciphertext)):
+        ch = ord(ciphertext[i])
+        if 65 <= ch <= 90:
+            if ch - shift < 65:
+                plaintext += chr(90 - (shift - (ch - 65) - 1))
+            else:
+                plaintext += chr(ch - shift)
+        elif 97 <= ch <= 122:
+            if ch - shift < 97:
+                plaintext += chr(122 - (shift - (ch - 97) - 1))
+            else:
+                plaintext += chr(ch - shift)
+        else:
+            plaintext += chr(ch)
+
     return plaintext
 
 
