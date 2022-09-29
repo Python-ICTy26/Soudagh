ef6a55762967934d4b4bc1d82b15da27fa812eb4
Soudagh <fr.kristina@inbox.ru>
2022-09-30 01:12:32 +0300

    Реализована функция поиска НОД

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -32,7 +32,12 @@ def gcd(a: int, b: int) -> int:
     1
     """
     # PUT YOUR CODE HERE
-    pass
+    while a != b:
+        if a > b:
+            a -= b
+        else:
+            b -= a
+    return a
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
@@ -55,10 +60,10 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
 
     # n = pq
     # PUT YOUR CODE HERE
-
+    n = p*q
     # phi = (p-1)(q-1)
     # PUT YOUR CODE HERE
-
+    phi = (p - 1)*(q - 1)
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
 
