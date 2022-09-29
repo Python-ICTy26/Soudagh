f2d812b595f3f41c316fc7f6eff4365eadbf5f8a
Soudagh <fr.kristina@inbox.ru>
2022-09-30 00:52:19 +0300

    Реализованы функции encrypt_vigenere(e) и decrypt_vigenere(0)

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -11,6 +11,33 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     ciphertext = ""
     # PUT YOUR CODE HERE
+
+    d = {}
+    start, end = ord('a'), ord('z') + 1
+    for i in range(start, end):
+        d[chr(i)] = i - start
+
+    while len(plaintext) > len(keyword):
+        keyword += keyword
+
+    keyword = keyword.lower()
+
+    for i in range(len(plaintext)):
+        ch = ord(plaintext[i])
+        shift = d[keyword[i]]
+        print(shift)
+        if 65 <= ch <= 90:
+            if ch + shift > 90:
+                ciphertext += chr(65 + (shift - (90 - ch)) - 1)
+            else:
+                ciphertext += chr(ch + shift)
+        elif 97 <= ch <= 122:
+            if ch + shift > 122:
+                ciphertext += chr(97 + (shift - (122 - ch)) - 1)
+            else:
+                ciphertext += chr(ch + shift)
+        else:
+            ciphertext += chr(ch)
     return ciphertext
 
 
@@ -27,4 +54,31 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+    d = {}
+    start, end = ord('a'), ord('z') + 1
+    for i in range(start, end):
+        d[chr(i)] = i - start
+    print(d)
+
+    while len(ciphertext) > len(keyword):
+        keyword += keyword
+
+    keyword = keyword.lower()
+
+    for i in range(len(ciphertext)):
+        ch = ord(ciphertext[i])
+        shift = d[keyword[i]]
+        if 65 <= ch <= 90:
+            if ch - shift < 65:
+                plaintext += chr(90 - (shift - (ch - 65) - 1))
+            else:
+                plaintext += chr(ch - shift)
+        elif 97 <= ch <= 122:
+            if ch - shift < 97:
+                plaintext += chr(122 - (shift - (ch - 97) - 1))
+            else:
+                plaintext += chr(ch - shift)
+        else:
+            plaintext += chr(ch)
+
     return plaintext
