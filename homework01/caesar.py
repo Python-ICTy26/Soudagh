fc8b4747324f488463f51f2469b840afab0bf9f9
Soudagh <fr.kristina@inbox.ru>
2022-09-30 17:27:39 +0300

    clean

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -2,18 +2,6 @@ import typing as tp
 
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
-    """
-    Encrypts plaintext using a Caesar cipher.
-
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
-    >>> encrypt_caesar("")
-    ''
-    """
     ciphertext = ""
     for i in range(len(plaintext)):
         ch = ord(plaintext[i])
@@ -34,20 +22,8 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
-    """
-    Decrypts a ciphertext using a Caesar cipher.
-
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
     for i in range(len(ciphertext)):
         ch = ord(ciphertext[i])
         if 65 <= ch <= 90:
@@ -67,9 +43,6 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
 
 
 def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
     best_shift = 0
 
     if ciphertext in dictionary:
@@ -79,5 +52,6 @@ def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
         plaintext = decrypt_caesar(ciphertext, best_shift)
         if plaintext in dictionary:
             return best_shift
+
         best_shift += 1
     return best_shift
--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,16 +1,5 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    """
-    Encrypts plaintext using a Vigenere cipher.
-
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
-    """
     ciphertext = ""
-    # PUT YOUR CODE HERE
 
     d = {}
     start, end = ord('a'), ord('z') + 1
@@ -42,18 +31,8 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    """
-    Decrypts a ciphertext using a Vigenere cipher.
-
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
-    """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
     d = {}
     start, end = ord('a'), ord('z') + 1
     for i in range(start, end):
