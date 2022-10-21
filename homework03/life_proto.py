d7b7adc975dca6d08401161b27f2b8f2afbe401e
Soudagh <fr.kristina@inbox.ru>
2022-10-21 18:19:24 +0300

    Реализован life_gui.py

--- homework03/life_proto.py
+++ homework03/life_proto.py
@@ -17,34 +17,26 @@ class GameOfLife:
         self.height = height
         self.cell_size = cell_size
 
-        # Устанавливаем размер окна
         self.screen_size = width, height
-        # Создание нового окна
         self.screen = pygame.display.set_mode(self.screen_size)
 
-        # Вычисляем количество ячеек по вертикали и горизонтали
         self.cell_width = self.width // self.cell_size
         self.cell_height = self.height // self.cell_size
 
-        # Скорость протекания игры
         self.speed = speed
 
     def draw_lines(self) -> None:
-        """ Отрисовать сетку """
         for x in range(0, self.width, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
 
     def run(self) -> None:
-        """ Запустить игру """
         pygame.init()
         clock = pygame.time.Clock()
         pygame.display.set_caption("Game of Life")
         self.screen.fill(pygame.Color("white"))
 
-        # Создание списка клеток
-        # PUT YOUR CODE HERE
         self.grid = self.create_grid(randomize=True)
 
         running = True
@@ -54,9 +46,6 @@ class GameOfLife:
                     running = False
             self.draw_lines()
 
-            # Отрисовка списка клеток
-            # Выполнение одного шага игры (обновление состояния ячеек)
-            # PUT YOUR CODE HERE
             self.draw_grid()
             pygame.display.flip()
             clock.tick(self.speed)
@@ -109,7 +98,3 @@ class GameOfLife:
                 elif grid[i][j] == 1 and (neighbours == 2 or neighbours == 3):
                     new_grid[i][j] = 1
         return new_grid
-
-if __name__ == '__main__':
-    game = GameOfLife(320, 240, 20)
-    game.run()
