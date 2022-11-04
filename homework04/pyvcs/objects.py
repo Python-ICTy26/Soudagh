d02887f59730341700508df00c37a5882e0882a3
Soudagh <fr.kristina@inbox.ru>
2022-11-04 18:51:13 +0300

    cat_files()

--- homework04/pyvcs/objects.py
+++ homework04/pyvcs/objects.py
@@ -29,18 +29,38 @@ def hash_object(data: bytes, fmt: str, write: bool = False) -> str:
 
 
 def resolve_object(obj_name: str, gitdir: pathlib.Path) -> tp.List[str]:
-    # PUT YOUR CODE HERE
-    ...
+    objs = []
+
+    if len(obj_name) > 40 or len(obj_name) < 4:
+        raise Exception(f"Not a valid object name {obj_name}")
+
+    blob_path = gitdir / "objects" / obj_name[:2]
+
+    for obj in blob_path.iterdir():
+        objs.append(find_object(obj_name, obj))
+
+    if not objs or objs[0] is None:
+        raise Exception(f"Not a valid object name {obj_name}")
+
+    return objs
 
 
 def find_object(obj_name: str, gitdir: pathlib.Path) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    if obj_name[2:] in gitdir.parts[-1]:
+        return str(gitdir.parts[-2] + str(gitdir.parts[-1]))
 
 
 def read_object(sha: str, gitdir: pathlib.Path) -> tp.Tuple[str, bytes]:
-    # PUT YOUR CODE HERE
-    ...
+    blob_path = gitdir / "objects" / sha[:2] / sha[2:]
+    with open(blob_path, mode="rb") as f:
+        blob_contents = zlib.decompress(f.read())
+
+    srez = blob_contents.find(b"\x00")
+    fmt = blob_contents[:srez]
+    fmt = fmt[:fmt.find(b" ")]
+    data = blob_contents[(srez + 1):]
+
+    return fmt.decode(), data
 
 
 def read_tree(data: bytes) -> tp.List[tp.Tuple[int, str, str]]:
@@ -49,8 +69,15 @@ def read_tree(data: bytes) -> tp.List[tp.Tuple[int, str, str]]:
 
 
 def cat_file(obj_name: str, pretty: bool = True) -> None:
-    # PUT YOUR CODE HERE
-    ...
+    gitdir = repo_find(pathlib.Path("."))
+
+    for obj in resolve_object(obj_name, gitdir):
+        header, data = read_object(obj, gitdir)
+
+    if pretty:
+        print(data.decode())
+    else:
+        print(data)
 
 
 def find_tree_files(tree_sha: str, gitdir: pathlib.Path) -> tp.List[tp.Tuple[str, str]]:
